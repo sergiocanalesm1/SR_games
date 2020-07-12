fc73ecf8dbb8dcc6b4f5d35dd8b5392a58a533c2
Sergio <sergiocanalesm1@gmail.com>
2020-07-12 05:31:59 -0500

    start short term memory

---
+++ shortTerm/jscript.js
@@ -0,0 +1,6 @@
+let data = { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" };
+let svg = d3.select("#canvas").append("svg")
+    .attr("width","500")
+    .attr("height","500")
+    ;
+
