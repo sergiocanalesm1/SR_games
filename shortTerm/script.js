8617a046bb363f4f42c47714ae04a51376cf4d4c
sergio <sergiocanalesm1@gmail.com>
2020-07-13 17:07:19 -0500

    buttons and dimentions fixed

--- shortTerm/script.js
+++ shortTerm/script.js
@@ -4,73 +4,103 @@ d3.select("#start-btn").on("click",renderGame);
 function renderGame(){
     d3.select("#instructions").style("display","none");
     d3.select("body").append("text").text("get ready!");
-    setTimeout(function(){
-        let radius = 25;
-        let width = 600;
-        let height = 300;
-        let svg = d3.select("#canvas")
-            .style("display","block")
-            .append("div") 
-            .classed("svg-container",true)
-            .append("svg")
-            .attr("preserveAspectRatio", "xMinYMin meet")
-            .attr("viewBox", `0 0 ${width} ${height}`)
-            .classed("svg-content-responsive", true)
-            ;
-        let lost = false;
-        let level = 3;
-        //while(!lost){
+    setTimeout(display_buttons,1000);
+}
 
-        let x = Math.ceil(Math.random()*width )
-        let y = Math.ceil(Math.random()*height)
-        let coordinates_buffer = [[x,y]];
-        for(let i=0; i<level;i++){
-            flag = true;
-            while(flag){ //so that no two coordinates are repeated
-                x = Math.ceil(Math.random()*width )
-                y = Math.ceil(Math.random()*height )
-                flag = false;
-                for(let j=0;j<coordinates_buffer.length;j++){
-                    let x1 = coordinates_buffer[j][0];
-                    let y1 = coordinates_buffer[j][1];
-                    let euclidean = Math.sqrt(Math.pow((x1-x),2)+Math.pow((y1-y),2));
-                    console.log(`current ${x},${y} buffer ${x1},${y1} distance ${euclidean}`);
-                    if( euclidean < 2*radius){
-                        flag = true;
-                        break;//next set of coordinates
-                    }//la hipotenusa tiene que ser >= 2R 
-                }
+function display_buttons(){
+
+    let radius = 25;
+    let width = 600;
+    let height = 200;
+    let svg = d3.select("#canvas")
+        .style("display","block")
+        .append("div") 
+        .classed("svg-container",true)
+        .append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", `0 0 ${width} ${height}`)
+        .classed("svg-content-responsive", true)
+        ;
+    let lost = false;
+    let level = 3;
+    //while(!lost){
+
+    let x = Math.ceil(Math.random()*(width-radius))+radius;
+    let y = Math.ceil(Math.random()*(height-radius))+radius;
+    let coordinates_buffer = [[x,y]];
+    for(let i=0; i<level;i++){
+        flag = true;
+        while(flag){ //so that no two coordinates are repeated
+            x = Math.ceil(Math.random()*(width-2*radius))+radius;
+            y = Math.ceil(Math.random()*(height-2*radius))+radius;
+            flag = false;
+            for(let j=0;j<coordinates_buffer.length;j++){
+                let x1 = coordinates_buffer[j][0];
+                let y1 = coordinates_buffer[j][1];
+                let euclidean = Math.sqrt(Math.pow((x1-x),2)+Math.pow((y1-y),2));
+                if( euclidean < 2*radius){
+                    flag = true;
+                    break;//next set of coordinates
+                }//la hipotenusa tiene que ser >= 2R 
             }
-            coordinates_buffer.push([x,y]);
-            console.log(coordinates_buffer);
-            svg.append("circle")
-                .attr("cx", x+"px")
-                .attr("cy", y+"px")
-                .attr("r", radius)
-                .attr("fill","white")
-                .style("stroke","black")
-                .attr("stroke-width","5")
-                ;
-                //create a button a draw it as a circle with css
-            svg.append("text")
-                .attr("x",x+"px")
-                .attr("y",y+"px")
-                .text(i+1)
-                .style("font-size", "40px")
-                .attr("text-anchor","middle")
-                .attr("alignment-baseline","central")
-                ;
         }
-
+        coordinates_buffer.push([x,y]);
         
+        svg.append("circle")
+            .attr("cx", x+"px")
+            .attr("cy", y+"px")
+            .attr("r", radius)
+            .attr("fill","white")
+            .style("stroke","black")
+            .attr("stroke-width","5")
 
-        setTimeout(function(){//hide numbers after 2 seconds
-            let numbers = d3.selectAll("text")
-                .style("display","none")
-                ;
-        },2000);
-    //}
-        },1000);
+            ;
+            //create a button a draw it as a circle with css
+        svg.append("text")
+            .attr("id",`circle_${i}`)
+            .attr("x",x+"px")
+            .attr("y",y+"px")
+            .text(i+1)
+            .style("font-size", "40px")
+            .attr("text-anchor","middle")
+            .attr("alignment-baseline","central")
+            ;
+    }
+
+    
+
+    setTimeout(function(){//hide numbers after 2 seconds
+        let numbers = d3.selectAll("text")
+            .style("display","none")
+            ;
+        let order = 0;
+        let circles = d3.selectAll("circle")
+            .on("mouseover",function(){
+                d3.select(this).style("opacity",'0.7'); 
+            })
+            .on("mouseout",function(){
+                d3.select(this).style("opacity","1");
+            })
+            .on("click",function(d,i){
+                if(i==order){
+                    d3.select(`#circle_${i}`).style("display","block");
+                    order++;
+                    
+                }
+                else{
+                    d3.select("body").append("text").text("you lost!");
+                    lost=true;
+                    d3.selectAll("circle").remove();
+                    d3.selectAll("text").remove();
+                }
+                if(order==level){
+                    console.log("ganaste perri");
+                    level++;
+                }
+            })
+
+    },200);
+//}
 }
 
 
